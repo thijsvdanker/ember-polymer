472842823016353291add15d87d9129a39c3253f
Thijs van den Anker <thijs@bitsnbolts.nl>
2015-06-04 10:04:46 +0200

    Extended example with poly-toolbar

--- app/router.js
+++ app/router.js
@@ -6,6 +6,8 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('poly-toolbar');
+  this.route('paper-toolbar');
 });
 
 export default Router;
---
+++ tests/unit/routes/paper-toolbar-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:paper-toolbar', 'Unit | Route | paper toolbar', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  var route = this.subject();
+  assert.ok(route);
+});
